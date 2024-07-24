c67cce8a9abdc1b9c22acf321b06e661aaa5abb4
pi <pi@klipperpi-MUjJiLh>
2024-07-24 10:39:38 -0400

    test.py modified - 24/07/24 - 10:39:34

--- printer_data/config/macros/test.py
+++ printer_data/config/macros/test.py
@@ -1,4 +1,4 @@
-ext_temp = printer.lookup_object('extruder').heater.get_temp()
-ext_target = printer.lookup_object('extruder').heater.get_target()
+ext_temp = printer.lookup_object('extruder').heater.get_temp(0)
+ext_target = printer.lookup_object('extruder').heater.get_target(0)
 
 print(f'{ext_temp}ºC/{ext_target}ºC')
