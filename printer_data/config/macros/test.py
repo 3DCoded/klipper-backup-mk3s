364762842c45178d800ce3a99779c484f5b50a44
pi <pi@klipperpi-MUjJiLh>
2024-07-24 10:42:40 -0400

    test.py modified - 24/07/24 - 10:42:36

--- printer_data/config/macros/test.py
+++ printer_data/config/macros/test.py
@@ -1,4 +1,4 @@
 ext_temp, ext_target = printer.lookup_object('extruder').heater.get_temp(0)
 ext_power = printer.lookup_object('extruder').heater.last_pwm_value
 
-print(f'Extruder: {round(ext_temp,2)}ºC/{ext_target}ºC, {ext_power}%')
+print(f'Extruder: {round(ext_temp,2)}ºC/{ext_target}ºC, {round(ext_power,2)*100}%')
