064cfbc369093279999a255871a05681766035dd
pi <pi@klipperpi-8a7703a>
2024-12-04 10:40:56 -0500

    test.py modified - 04/12/24 - 10:40:51

--- printer_data/config/macros/test.py
+++ printer_data/config/macros/test.py
@@ -2,7 +2,7 @@ import threading
 
 print('Hello before')
 
-t = threading.Thread(target=gcode, args=('M109 S30',))
+t = threading.Thread(target=gcode, args=('M109 S50',))
 t.run()
 
 print('Hello after')
